884c1472d267b2535e7ca37054cc4de2d17c8df8
lutir <rituljain.nitt@gmail.com>
2020-02-24 15:52:43 -0600

    testing connection

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Capital One
                 </div>
 
                 <div class="links">
